eda716fe646e268af762507d556f12d99dbb86cd
Pawel Kikh <pavlokiht2005@gmail.com>
2023-11-29 16:55:14 +0100

    Add blogapp URLs and views

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status = 'published')
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -18,7 +21,16 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now = True)
     status = models.CharField(max_length = 10, choices = STATUS_CHOICES, default = 'draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+    
+
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
+    
+    
