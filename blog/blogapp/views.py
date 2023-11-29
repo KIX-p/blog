d43d4785aed7d06c7cd17f9964b6b0dde2b80fb1
Pawel Kikh <pavlokiht2005@gmail.com>
2023-11-29 20:46:06 +0100

    Add include statement to urls.py and create base.html and pagination.html templates

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 # Create your views here.
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blogapp/post/list.html', {'posts':posts})
+    
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blogapp/post/list.html', {'posts':posts, 'page':page})
+    
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug = post, status = 'published', publish__year = year, publish__month = month, publish__day = day)
     return render(request, 'blogapp/post/detail.html', {'post':post})
+
