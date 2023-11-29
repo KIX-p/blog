b1a50b9dd2dd73381a1de350f6c808d9035f8f46
Pawel Kikh <pavlokiht2005@gmail.com>
2023-11-29 21:16:56 +0100

    Refactor post_list view to use class-based view

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,24 +1,15 @@
+from msilib.schema import ListView
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
 # Create your views here.
 
-def post_list(request):
-    posts = Post.published.all()
-    
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blogapp/post/list.html', {'posts':posts, 'page':page})
-    
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blogapp/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug = post, status = 'published', publish__year = year, publish__month = month, publish__day = day)
